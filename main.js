4c411184ba77d07ddbd18582c2974745e774ee80
isavmv <isadora.varoni@escola.pr.gov.br>
2024-10-08 09:39:03 -0300

    3tri8

---
+++ main.js
@@ -0,0 +1,20 @@
+const caixaPrincipal = document.querySelector("caixaPrincipal")
+const caixaPerguntas = document.querySelector("caixaPerguntas")
+const caixaAlternativas = document.querySelector("caixaAlternativas")
+const caixaresultado = document.querySelector("caixa-resultado")
+const textoResultado = document.querySelector("texto-resultado")
+
+const Perguntas = [
+    {
+         enunciado: "insira o ennciado da pergunta 1",
+         alternativas: ["Alternativa 1", "alternativa 2"],
+    },
+    {
+        enunciado: "Insira o enunciado da pergunta2",
+        alternativas:["alternativa1","alternativa2"],
+    },
+    {
+        enunciado:"insira o enunciado da pergunta3",
+        alternativas: ["alternativa1","alternativa2"],
+    }
+];
